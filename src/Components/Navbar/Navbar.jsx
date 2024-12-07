5c89fa9901ee58911b16b0be888d9e0f4d2ddd13
Bawa Ernest Kwame <bawaernest926@gmail.com>
2024-12-07 23:36:13 +0000

    side nav responsiveness

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 import ContactPhoneIcon from '@mui/icons-material/ContactPhone';
 import { Link, NavLink } from 'react-router-dom';
@@ -6,85 +6,56 @@ import MenuIcon from '@mui/icons-material/Menu';
 import ClearIcon from '@mui/icons-material/Clear';
 
 const Navbar = () => {
+  const [menuBtn, setmenuBtn] = useState(false);
+
+  const toggleMenu = () => {
+    setmenuBtn(!menuBtn);
+  };
+
+  const closeMenu = () => {
+    setmenuBtn(false);
+  };
+
   return (
     <>
-    <div className='navbar'>
-      <div className="logo">
-       <Link to="/"> <h1>Be<span>Code</span></h1></Link>
-      </div>
-      <nav>
-        <ul>
-          <li>
-            <NavLink to="/">HOME</NavLink>
-          </li>
-          <li>
-            <NavLink to="/ABOUT">ABOUT</NavLink>
-          </li>
-          <li>
-            <NavLink to="/COURSES" >COURSES</NavLink>
-          </li>
-          <li>
-            <NavLink to="/CONTACT" >CONTACT</NavLink>
-          </li>
-        </ul>
-
-        <div className="loginsignups">
-          <Link to="/Login"><button className='login-btn btn'>Login</button></Link>
-          <Link to="/Signup"><button className='signup-btn btn'>Sign Up</button></Link>
+      <div className='navbar'>
+        <div className="logo">
+          <Link to="/"> <h1>Be<span>Code</span></h1></Link>
         </div>
 
-        <div className="contact">
-          <div className="contact-icon">
-            <ContactPhoneIcon />
+        <div className='btns menu-btn' onClick={toggleMenu}>
+          {menuBtn ? <ClearIcon /> : <MenuIcon />}
+        </div>
+
+        <nav>
+          <ul className={menuBtn ? "open" : ""}>
+            <li>
+              <NavLink to="/" onClick={closeMenu}>HOME</NavLink>
+            </li>
+            <li>
+              <NavLink to="/ABOUT" onClick={closeMenu}>ABOUT</NavLink>
+            </li>
+            <li>
+              <NavLink to="/COURSES" onClick={closeMenu}>COURSES</NavLink>
+            </li>
+            <li>
+              <NavLink to="/CONTACT" onClick={closeMenu}>CONTACT</NavLink>
+            </li>
+          </ul>
+
+          <div className="loginsignups">
+            <Link to="/Login"><button className='login-btn btn'>Login</button></Link>
+            <Link to="/Signup"><button className='signup-btn btn'>Sign Up</button></Link>
           </div>
-          <p>+233599316218</p>
-        </div>
-      </nav>
 
-     
-        <div className='btns menu-btn'><MenuIcon/></div>
-       
-   
-    </div>
-
-    <div className='sideNave'>
-
-
-      <div className="logo">
-       <Link to="/"> <h1>Be<span>Code</span></h1></Link>
-       <div className='btns close-btn'><ClearIcon/></div>
-   
+          <div className="contact">
+            <div className="contact-icon">
+              <ContactPhoneIcon />
+            </div>
+            <p>+233599316218</p>
+          </div>
+        </nav>
       </div>
-      <nav>
-        <ul>
-        
-          <li>
-            <NavLink to="/">HOME</NavLink>
-          </li>
-          <li>
-            <NavLink to="/ABOUT">ABOUT</NavLink>
-          </li>
-          <li>
-            <NavLink to="/COURSES" >COURSES</NavLink>
-          </li>
-          <li>
-            <NavLink to="/CONTACT" >CONTACT</NavLink>
-          </li>
-        </ul>
-
-        <div className="loginsignup">
-          <Link to="/Login"><button className='login-btn btn'>Login</button></Link>
-          <Link to="/Signup"><button className='signup-btn btn'>Sign Up</button></Link>
-        </div>
-
-   
-      </nav>
-
-     
-       
-   
-    </div>
-
     </>
   );
 };
