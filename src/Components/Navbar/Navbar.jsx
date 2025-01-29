12e5927c44d6ad71c9173120ea463fdeabb482bb
Bawa Ernest Kwame <bawaernest926@gmail.com>
2025-01-29 22:37:19 +0000

    genera nav update

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,58 +6,37 @@ import MenuIcon from '@mui/icons-material/Menu';
 import ClearIcon from '@mui/icons-material/Clear';
 
 const Navbar = () => {
-  const [menuBtn, setmenuBtn] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const toggleMenu = () => {
-    setmenuBtn(!menuBtn);
-  };
-
-  const closeMenu = () => {
-    setmenuBtn(false);
+    setMenuOpen(!menuOpen);
   };
 
   return (
-    <>
-      <div className='navbar'>
-        <div className="logo">
-          <Link to="/"> <h1>Be<span>Code</span></h1></Link>
-        </div>
-
- <div className="contact">
-            <div className="contact-icon">
-              <ContactPhoneIcon />
-            </div>
-            <p>+233599316218</p>
-          </div>
-        <div className='btns menu-btn' onClick={toggleMenu}>
-          {menuBtn ? <ClearIcon /> : <MenuIcon />}
-
-         
-        </div>
-
-        <nav>
-          <ul className={menuBtn ? "open" : ""}>
-            <li>
-              <NavLink to="/" onClick={closeMenu}>HOME</NavLink>
-            </li>
-            <li>
-              <NavLink to="/ABOUT" onClick={closeMenu}>ABOUT</NavLink>
-            </li>
-            <li>
-              <NavLink to="/COURSES" onClick={closeMenu}>COURSES</NavLink>
-            </li>
-            <li>
-              <NavLink to="/CONTACT" onClick={closeMenu}>CONTACT</NavLink>
-            </li>
-            <div className="loginsignups">
-            <Link to="/Login"><button className='login-btn btn'>Login</button></Link>
-            <Link to="/Signup"><button className='signup-btn btn'>Sign Up</button></Link>
-          </div>
-            
-          </ul>
-        </nav>
+    <header className='navbar'>
+      <div className='logo'>
+        <Link to='/'>
+          <h1>Be<span>Code</span></h1>
+        </Link>
       </div>
-    </>
+
+      <nav>
+        <ul className={menuOpen ? 'nav-links open' : 'nav-links'}>
+          <li><NavLink to='/' onClick={() => setMenuOpen(false)}>HOME</NavLink></li>
+          <li><NavLink to='/about' onClick={() => setMenuOpen(false)}>ABOUT</NavLink></li>
+          <li><NavLink to='/courses' onClick={() => setMenuOpen(false)}>COURSES</NavLink></li>
+          <li><NavLink to='/contact' onClick={() => setMenuOpen(false)}>CONTACT</NavLink></li>
+          <div className='auth-buttons'>
+            <Link to='/login'><button className='login-btn'>Login</button></Link>
+            <Link to='/signup'><button className='signup-btn'>Sign Up</button></Link>
+          </div>
+        </ul>
+      </nav>
+
+      <div className='menu-icon' onClick={toggleMenu}>
+        {menuOpen ? <ClearIcon /> : <MenuIcon />}
+      </div>
+    </header>
   );
 };
 
